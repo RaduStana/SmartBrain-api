2129e76246833bc285248720b556a420376fac1e
Stana Radu <stana.radu@gmail.com>
2024-04-12 17:23:33 +0300

    change response from ClarifaiAPI

--- controllers/image.js
+++ controllers/image.js
@@ -33,7 +33,7 @@ const handleApiCall = (req,res) => {
 	fetch(`https://api.clarifai.com/v2/models/face-detection/outputs`,returnClarifaiRequestOption(req.body.input))
 	.then(response => response.json())
 	.then(data => {
-		res.json(data);
+		res.json(data.outputs[0].data.regions[0].region_info.bounding_box);
 	})
 	.catch(err => res.status(400).json('unable to work with API'))
 }
